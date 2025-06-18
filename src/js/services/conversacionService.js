c3ecea48786d0692203dc7aed2aea765080e2d1b
bryan <programador07@vip2cars.com>
2025-06-17 19:06:24 -0500

    funcionando

--- src/js/services/conversacionService.js
+++ src/js/services/conversacionService.js
@@ -2,11 +2,20 @@ import api from "../api.js";
 
 export async function getConversaciones() {
   const res = await api.get("/conversacion/listadoTelefonos");
+  // console.log("Conversaciones obtenidas:", res.data.data);
   return res.data.data;
 }
 
 export async function aceptarConversacion(conversacionId, usuarioId) {
   const res = await api.post("/conversacion/aceptar", {
+    conversacionId,
+    usuarioId,
+  });
+  return res.data;
+}
+
+export async function finalizarConversacion(conversacionId , usuarioId) {
+  const res = await api.post("/conversacion/finalizar", {
     conversacionId,
     usuarioId,
   });
