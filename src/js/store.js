5a156f34a8264b1c9c66d82477a825b0d00eccc4
CoronadoBryan <smithelunico1234@gmail.com>
2025-06-20 19:14:45 -0500

    impelmentado en casa auth

--- src/js/store.js
+++ src/js/store.js
@@ -1,35 +1,124 @@
-
 import { createStore } from 'framework7';
+import tools from './tools.js';
+import moment from 'moment';
 
 const store = createStore({
-  state: {
-    products: [
-      {
-        id: '1',
-        title: 'Apple iPhone 8',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-      },
-      {
-        id: '2',
-        title: 'Apple iPhone 8 Plus',
-        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-      },
-      {
-        id: '3',
-        title: 'Apple iPhone X',
-        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-      },
-    ]
-  },
-  getters: {
-    products({ state }) {
-      return state.products;
-    }
-  },
-  actions: {
-    addProduct({ state }, product) {
-      state.products = [...state.products, product];
+    state: {
+        login: false,
+        user: {},
+        perfil: {}
     },
-  },
+    getters: {
+        sesionIniciada( {state}) {
+            return state.login;
+        },
+        cuenta( {state}){
+            return state.user;
+        },
+        perfil( {state}){
+            return state.perfil;
+        },
+        isSudo( {state}){
+            return state.user.maestro === 1;
+        },
+        token() {
+            return localStorage.getItem('token');
+        },
+    },
+    actions: {
+        setUserInfo( {state, dispatch}, tokenDecode){
+            let userInfo = tools.sendRequest('GET', '/usuario/logueado');
+
+            userInfo.then((data) => {
+                if (data.success) {
+                    let datos = data['data'];
+
+                    state.login = (tokenDecode['sub'] === datos['id']);
+
+                    if (state.login) {
+                        state.user = {
+                            'id_user': datos.id,
+                            'maestro': datos.sudo,
+                            'usuario': datos.usuario,
+                            'activo': datos.activo,
+                            'bloqueado': datos.cuenta_bloqueada,
+                            //'force_pass': datos.forzar_cambio_pw,
+                            'force_pass': 1,
+
+                        };
+
+                        state.perfil = {
+                            'nombres': datos.perfil['nombres'],
+                            'apellidos': datos.perfil['apellidos'],
+                            'nro_documento': datos.perfil['nro_documento'],
+                            'correo': datos.perfil['correo'],
+                            'celular': datos.perfil['celular'],
+                        };
+
+                        /*RETORNAR*/
+                        if(datos.forzar_cambio_pw==1){
+                            return dispatch('/cambiar-password/');
+
+                        }else{
+                            return dispatch('abrirSistema');
+
+                        } 
+                    }
+                }
+
+                dispatch('cerrarSistema');
+            });
+        },
+        setToken( {state, dispatch}, token){
+            if (token === null) {
+                dispatch('cerrarSistema');
+
+                return false;
+            }
+
+            try {
+                let tokenDecode = tools.parseJwt(token);
+
+                if (tokenDecode === false) {
+                    throw new Error('TOKEN invalido.');
+                }
+
+                let token_emision = tokenDecode.iat;
+                let token_noantes = tokenDecode.nbf;
+                let token_expire = tokenDecode.exp;
+                let momento_actual = moment().unix();
+
+                if (token_emision === token_noantes && token_noantes <= momento_actual && momento_actual <= token_expire) {
+                    localStorage.setItem('token', token);
+
+                    dispatch('setUserInfo', tokenDecode);
+
+                    return (tokenDecode !== false);
+                } else {
+                    throw new Error('Sesión expirada.');
+                }
+            } catch (ex) {
+                dispatch('cerrarSistema');
+
+                if (token !== null) {
+                    app.f7.notification.create({text: ex}).open();
+                }
+            }
+
+            return false;
+        },
+        abrirSistema( {state}){
+            app.f7.loginScreen.close();
+        },
+        cerrarSistema( {state}){
+//            state.token = null;
+            state.user = {};
+            state.perfil = {};
+
+            localStorage.removeItem("token");
+
+            app.f7.loginScreen.open('#login-screen');
+        },
+    },
 })
 export default store;
---
+++ src/js/tools.js
@@ -0,0 +1,107 @@
+import config from './api.js';
+import moment from 'moment';
+
+const tools = {
+    isJSON: function (jsonString) {
+        if (!!jsonString && typeof jsonString === "object") {
+            return jsonString;
+        }
+
+        try {
+            let obj = JSON.parse(jsonString);
+
+            if (obj && typeof obj === "object") {
+                return obj;
+            }
+        } catch (e) {
+        }
+
+        return false;
+    },
+    sendRequest: function (method, uri, data) {
+        try {
+            (!method) && (method = 'GET');
+            (!uri) && (uri = '');
+            let token = localStorage.getItem('token');
+            let body = null;
+
+            if (String(uri).length === 0) {
+                throw new Error('El URI no puede ser vacio.');
+            }
+
+            if ((data = this.isJSON(data)) !== false) {
+                body = JSON.stringify(data);
+            }
+
+            if (!token) {
+                throw new Error('El TOKEN no existe.');
+            }
+
+            let request = new Request(config.endpoint(uri), {
+                method: method,
+                headers: new Headers({
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }),
+                body: body
+            });
+
+            let peticion = fetch(request).then((response) => {
+                return response.json();
+            });
+
+            return peticion;
+        } catch (ex) {
+            console.error('sendRequest', ex.message);
+            return Promise.reject(ex);
+        }
+    },
+    parseJwt: function (token) {
+        try {
+            let base64Url = token.split('.')[1];
+            let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+
+            return JSON.parse(jsonPayload);
+        } catch (e) {
+            console.error('parseJwt', e);
+        }
+
+        return false;
+    },
+    addRoutes: function (routes_base, routes_new) {
+        let nuevos = routes_new.filter((route) => {
+            let existe = routes_base.find(({path}) => {
+                return path === route['path'];
+            });
+
+            return !this.isJSON(existe);
+        });
+
+        nuevos.map((route) => {
+            routes_base.push(route);
+        });
+    },
+    getDocumentos: function(){
+        return this.sendRequest('GET', '/documento/listado').then((response) => {
+            if (response && response.success == true) {
+                return response.data;
+            }
+        });
+    },
+    getDatabyDocument: function (documento) {
+        return this.sendRequest('GET', `/usuario/buscar/${documento}`)
+            .then(response => {
+                if (response.success == true) {
+                    return response.data;
+                }else {
+                    return null
+                }
+            });
+    },
+    moment
+};
+
+export default tools;
