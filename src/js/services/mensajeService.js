3bc6ec0eb56b759ea7d79d6748886761f3410ba9
bryan <programador07@vip2cars.com>
2025-07-02 17:47:59 -0500

    funcionando el reenvio de mensajes

--- src/js/services/mensajeService.js
+++ src/js/services/mensajeService.js
@@ -1,17 +1,28 @@
 import config from "../config.js";
 
 export async function getMensajesPorConversacion(conversacionId) {
-  const res = await config.api.get(`/mensaje/listar-por-conversacion/${conversacionId}`);
+  const res = await config.api.get(
+    `/mensaje/listar-por-conversacion/${conversacionId}`
+  );
   console.log("Respuesta de mensajes:", res.data);
   return res.data.data;
 }
 
 export async function enviarMensaje(payload) {
-  const res = await config.api.post("/mensaje/crear-y-enviar-whatsapp", payload);
+  const res = await config.api.post(
+    "/mensaje/crear-y-enviar-whatsapp",
+    payload
+  );
   return res.data;
 }
 
 export async function mensajeLeido(mensajeId) {
   const res = await config.api.put(`/mensaje/marcar-leido/${mensajeId}`);
+  return res.data;
+}
+
+export async function reenviarMensaje(mensajeId) {
+  const res = await config.api.post(`/mensaje/reenviar/${mensajeId}`);
+  console.log(`Reenviando mensaje con ID: ${mensajeId}`, res.data);
   return res.data;
 }
