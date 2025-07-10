4073fe116c5cc067a63a730c8b314c0e4141fbae
CoronadoBryan <smithelunico1234@gmail.com>
2025-07-10 15:42:10 -0500

    todo funcionado con atajos

--- src/js/services/mensajeService.js
+++ src/js/services/mensajeService.js
@@ -27,9 +27,21 @@ export async function reenviarMensaje(mensajeId) {
   return res.data;
 }
 
-
 export async function getPlantillasPorCategoria() {
   const res = await config.api.get("/mensaje/plantilla/listarPorCategoria");
   console.log("Respuesta de plantillas:", res.data);
   return res.data;
+}
+
+export async function getAtajos() {
+  const res = await config.api.get("/mensaje/atajo/listado");
+  return res.data.data;
+}
+
+export async function reemplazarAtajosEnMensaje(mensaje, id_conversacion) {
+  const res = await config.api.post(
+    "/mensaje/atajo/probar-reemplazo",
+    { mensaje, id_conversacion }
+  );
+  return res.data.procesado || mensaje;
 }
